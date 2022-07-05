aa3beb4cb8c738c34fb546dd5e3d1a9cd3746fc1
Farhana100 <farhanakhanhp@gmail.com>
2022-07-05 20:57:16 +0600

    test

--- contest_arena/urls.py
+++ contest_arena/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('leaderboard', view_leaderboard_page, name='leaderboard'),
     path('admin_leaderboard', view_admin_leaderboard_page, name='admin_leaderboard'),
-    path('puzzle/<int:pk>', load_next_puzzle, name='puzzle'),
+    path('puzzle/<int:pk>/', load_next_puzzle, name='puzzle'),
     path('hackerman', hack, name='hackerman'),
     path('banned', banned, name='banned'),
 ]
